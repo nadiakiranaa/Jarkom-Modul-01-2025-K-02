79ea19ba56e592542b85f571b399a2e8c7a1eb66
nadiakiranaa <nadiakirana280@gmail.com>
2025-10-01 22:26:02 +0700

    Add files via upload

---
+++ soal_19.sh
@@ -0,0 +1,16 @@
+nc 10.15.43.32 3406
+
+A. Who sent the threatening message?
+Format: string (name)
+> Your Life
+
+B. How much ransom did the attacker demand ($)?
+Format: int
+> 1600
+
+C. What is the attacker's bitcoin wallet?
+Format: string
+> 1CWHmuF8dHt7HBGx5RKKLgg9QA2GmE3UyL
+
+Congratulations! Here is your flag: KOMJAR25{Y0u_4re_J4rk0m_G0d_giB0vHez4PXkT2GqL26TkN46T}
+
