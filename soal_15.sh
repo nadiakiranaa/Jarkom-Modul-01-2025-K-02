2e4f661b25b2a2b0a361001afdd419c38cff51e4
nadiakiranaa <nadiakirana280@gmail.com>
2025-10-01 22:45:08 +0700

    Add files via upload

---
+++ soal_15.sh
@@ -0,0 +1,16 @@
+nc 10.15.43.32 3402
+
+A. What device does Melkor use?
+Format: string
+> keyboard
+
+B. What did Melkor write?
+Format: string
+> UGx6X3ByMHYxZGVfeTB1cl91czNybjRtZV80bmRfcDRzc3cwcmQ=
+
+
+C. What is Melkor's secret message?
+Format: string
+>  Plz_pr0v1de_y0ur_us3rn4me_4nd_p4ssw0rd
+
+ Congratulations! Here is your flag: KOMJAR25{K3yb0ard_W4rr10r_FvWwfBk7NK9BjE7M8i4rbqIma}
