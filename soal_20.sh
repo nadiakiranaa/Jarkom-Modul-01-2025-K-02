6550d5143e87532f67216703732c80834b303a7a
nadiakiranaa <nadiakirana280@gmail.com>
2025-10-01 22:26:17 +0700

    Add files via upload

---
+++ soal_20.sh
@@ -0,0 +1,18 @@
+nc 10.15.43.32 3407
+
+A. What encryption method is used?
+Format: string
+> TLS
+
+B. What is the name of the malicious file placed by the attacker?
+Format: file.exe
+> invest_20.dll
+
+- sha256sum invest_20.dll
+31cf42b2a7c5c558f44cfc67684cc344c17d4946d3a1e0b2cecb8eb58173cb2f  invest_20.dll
+
+C. What is the hash of the file containing the malware?
+Format: sha256
+> 31cf42b2a7c5c558f44cfc67684cc344c17d4946d3a1e0b2cecb8eb58173cb2f
+
+Congratulations! Here is your flag: KOMJAR25{B3ware_0f_M4lw4re_VYIGjY2atGovFvy77jv0OL2nl}
