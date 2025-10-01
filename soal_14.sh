f588fd89a4a3b8ed414f82cdc50c489d67bcf549
nadiakiranaa <nadiakirana280@gmail.com>
2025-10-01 22:03:34 +0700

    Update soal_14.sh

--- soal_14.sh
+++ soal_14.sh
@@ -1,15 +1,16 @@
 nc 10.15.43.32 3401
-// How many packets are recorded in the pcapng file? //
-//500358 //
+A. How many packets are recorded in the pcapng file? 
+- 500358
 
-// What are the user that successfully logged in?
-Format: user:pass //
-// n1enna:y4v4nn4_k3m3nt4r1//
+B. What are the user that successfully logged in?
+  Format: user:pass 
+- n1enna:y4v4nn4_k3m3nt4r1
 
-// What tools are used for brute force?
-Format: Hydra v1.8.0-dev//
-//  Fuzz Faster U Fool v2.1.0-dev// 
+C. What tools are used for brute force?
+Format: Hydra v1.8.0-dev
+- Fuzz Faster U Fool v2.1.0-dev
 
 Congratulations! Here is your flag: KOMJAR25{Brut3_F0rc3_yqjl9r5dJfAWjACVWGFacFvaZ}
+
 
 
