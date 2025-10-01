876c2170ff24c0968ae493f96b09b0e63b2a45f5
nadiakiranaa <nadiakirana280@gmail.com>
2025-10-01 22:13:25 +0700

    Add files via upload

---
+++ soal_17.sh
@@ -0,0 +1,18 @@
+nc 10.15.43.32 3404
+
+A. What is the name of the first suspicious file?
+Format: file.exe
+> Invoice&MSO-Request.doc
+
+B. What is the name of the second suspicious file?
+Format: file.exe
+> knr.exe
+
+- sha256sum knr.exe
+749e161661290e8a2d190b1a66469744127bc25bf46e5d0c6f2e835f4b92db18  knr.exe
+
+C. What is the hash of the second suspicious file (knr.exe)?
+Format: sha256
+> 749e161661290e8a2d190b1a66469744127bc25bf46e5d0c6f2e835f4b92db18
+
+Congratulations! Here is your flag: KOMJAR25{M4ster_4n4lyzer_qiVe00FU26crBEivYtTdqUPSc}
